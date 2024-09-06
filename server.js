c6d7b852a631d4173b7909d441c5f6c8f16edbf2
Oscar Hermawan <oscar.hermawan90@gmail.com>
2024-09-07 00:45:18 +0800

    Change - Cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: "http://localhost:5000",
+    origin: "http://localhost:6000",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
