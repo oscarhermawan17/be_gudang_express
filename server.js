accf815e40bf3b34b8193d32d9b71b8271c464d1
Oscar Hermawan <oscar.hermawan90@gmail.com>
2024-09-07 01:39:02 +0800

    BackEnd - Remove CORS

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 
 const express = require("express")
-const cors = require("cors")
+// const cors = require("cors")
 
 const { sequelize } = require("./models")
 const authRoutes = require("./routes/authRoutes")
@@ -9,19 +9,19 @@ const productRoutes = require("./routes/productRoutes")
 
 const app = express()
 app.use(express.json())
-app.use(
-  cors({
-    origin: "http://localhost:6000",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-)
+// app.use(
+//   cors({
+//     origin: "http://localhost:6000",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true,
+//   })
+// )
 
 // Debug CORS
-app.use((req, res, next) => {
-  console.log("CORS middleware executed")
-  next()
-})
+// app.use((req, res, next) => {
+//   console.log("CORS middleware executed")
+//   next()
+// })
 
 app.use("/auth", authRoutes)
 
